65ad2e3465092eb3e4d54a0923170fa23044a43b
incrl <hartda23@ecu.edu>
2024-03-22 14:28:50 -0400

    Fixed statistics.py added Documentation

--- UnifiedFramework/statistics.py
+++ UnifiedFramework/statistics.py
@@ -1,6 +1,7 @@
 import numpy as np
 import cv2 as cv
-import json
+#import json
+import pickle
 import argparse
 from math import sqrt
 
@@ -16,8 +17,8 @@ def calcAverageSpeed(centroids, visible, fps=30):
 
     for i in range(len(visible)):
         if visible[i] == 1 and visible[i-1] == 1 and i-1 >= 0:
-            start = centroids[str(i-1)]
-            end = centroids[str(i)]
+            start = centroids[i-1]
+            end = centroids[i]
             total += sqrt((end[1]-start[1])**2 + (end[0]-start[0])**2)
 
     # Calculate the average speed
@@ -28,13 +29,13 @@ def calcAverageSpeed(centroids, visible, fps=30):
 
 
 parser = argparse.ArgumentParser(description='Compute statistics about sperm cells')
-parser.add_argument('jsonfile', type=str, help='Path to the tracker json file')
+parser.add_argument('pklfile', type=str, help='Path to the tracker pkl file')
 
-jsonfile = parser.parse_args().jsonfile
+pklfile = parser.parse_args().pklfile
 
 # Load the json file
-with open(jsonfile, 'r') as f:
-    data = json.load(f)
+with open(pklfile, 'rb') as f:
+    data = pickle.load(f)
 
 stats = {}
 
@@ -50,9 +51,9 @@ for i in range(len(data)):
     stats[i]["average_speed"] = average_speed
 
 # Save the stats to a json file
-outputfile = jsonfile.split('.')[0][:-8] + '_stats.json'
+outputfile = pklfile.split('.')[0][:-8] + '_stats.pkl'
 
-with open(outputfile, 'w') as f:
-    json.dump(stats, f)
+with open(outputfile, 'wb') as f:
+    pickle.dump(stats, f)
 
 print(f'Saved statistics to {outputfile}')
