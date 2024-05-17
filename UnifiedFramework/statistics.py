9d63d48c38f0164eb4d910c32a49456b8678742d
cashwellk16 <163585096+cashwellk16@users.noreply.github.com>
2024-05-17 14:36:25 -0400

    Add Average Path velocity
    Add a function to statistics.py that calculates average path velocity and changed it so that the stat data pkl saves this calculation instead.

--- UnifiedFramework/statistics.py
+++ UnifiedFramework/statistics.py
@@ -27,6 +27,39 @@ def calcAverageSpeed(centroids, visible, fps=30):
     return average_speed
 
 
+def averagePathVelocity(centroids, visible, pix_size, win_size, fps=5):
+    """
+    Calculate the average path velocity (VAP)
+    FPS frames per a second
+    pixel size is determined by pix_size
+    Window size is determined by win_size and is a user input for the units of frame to include in the averaging
+    argument for VCl and VAP calculations
+    """
+    # Initialize total distance and count of valid frames
+    total_distance = 0
+    valid_frame_count = 0
+
+    # Iterate over visible frames to calculate distances
+    for i in range(1, len(visible)):
+        if visible[i] == 1 and visible[i - 1] == 1:
+            start = centroids[i - 1]
+            end = centroids[i]
+            distance = sqrt((end[1] - start[1]) ** 2 + (end[0] - start[0]) ** 2)
+            total_distance += distance
+            valid_frame_count += 1
+            print(f"Distance from {start} to {end}: {distance}")
+
+    print(f"Total distance: {total_distance}")
+
+    if valid_frame_count > 0:
+        # Calculate the VAP for microns per second
+        velocity = (total_distance / valid_frame_count) * fps * pix_size
+    else:
+        velocity = 0
+
+    print(f"Velocity: {velocity}")
+    return velocity
+
 
 parser = argparse.ArgumentParser(description='Compute statistics about sperm cells')
 parser.add_argument('pklfile', type=str, help='Path to the tracker pkl file')
@@ -45,7 +78,7 @@ for i in range(len(data)):
     centroids = data[i]['centroid']
     visible = data[i]['visible']
 
-    average_speed = calcAverageSpeed(centroids,visible)
+    average_speed = averagePathVelocity(centroids, visible, 0.26, 5)
 
     stats[i] = {}
     stats[i]["average_speed"] = average_speed
