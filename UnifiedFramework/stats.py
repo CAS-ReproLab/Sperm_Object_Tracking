ccddf1073661407551259caa97af3cf6e59b75c1
cashwellk16 <163585096+cashwellk16@users.noreply.github.com>
2024-06-20 14:28:24 -0400

    Added Curvilinear velocity

--- UnifiedFramework/stats.py
+++ UnifiedFramework/stats.py
@@ -97,6 +97,58 @@ def averagePathVelocity(data, fps=30, pixel_size = 0.26, win_size= 1):
     return data
 
 
+def curvilinearVelocity(data, fps=30, pixel_size = 0.26):
+    '''Calculate the average path velocity (VAP) over all frames
+
+    Parameters:
+    data (pd.DataFrame): DataFrame containing sperm tracking data with columns 'sperm', 'frame', 'x', and 'y'.
+    fps (int): Frames per second, default is 30.
+    pixel_size (float): Size of one pixel in micrometers (or any other unit), default is 0.26.
+    win_size (int): Size of the window in micrometers (or any other unit), default is 1 aka calculates
+    curvilinear velocity (VCL). Ajust window_size accordingly for VAP.
+    Returns the pd.DataFrame: DataFrame with an additional column 'VAP' containing the average speed of each sperm cell.
+    '''
+    # Add a column to the dataframe to store the average speed
+    data['VCL'] = 0.0
+
+    # Determine the number of sperm
+    sperm_count = data['sperm'].nunique()
+
+
+    # Iterate over each sperm
+    for i in range(sperm_count):
+        # Filter the dataframe for the current sperm
+        sperm = data[data['sperm'] == i]
+
+        # Sort the dataframe by frame
+        sperm = sperm.sort_values(by='frame')
+
+        # Calculate the total distance traveled by the sperm
+        total_distance = 0
+        valid_frame_count = 0
+        for j in range(1, len(sperm)):
+            start = (sperm['x'].iloc[j - 1], sperm['y'].iloc[j - 1])
+            end = (sperm['x'].iloc[j], sperm['y'].iloc[j])
+            # Calculate the distance in pixels
+            distance_pixels = sqrt((end[1] - start[1]) ** 2 + (end[0] - start[0]) ** 2)
+
+            # Add total distance
+            total_distance += distance_pixels
+
+            valid_frame_count += 1
+
+        if valid_frame_count > 0:
+            # Calculate the VCL for microns per second
+            velocity =  total_distance * (fps/ (valid_frame_count-1)) * pixel_size
+        else:
+            velocity = 0
+
+        # Add the average speed to the dataframe
+        data.loc[data['sperm'] == i, 'VCL'] = velocity
+
+    return data
+
+
 
 
 if __name__ == '__main__':
@@ -109,12 +161,14 @@ if __name__ == '__main__':
     data = utils.loadDataFrame(csvfile)
 
     # Run calcAverageSpeed
-    average_speed = averagePathVelocity(data, fps= 30, pixel_size= 0.26, win_size= 5)
+    vap = averagePathVelocity(data, fps= 30, pixel_size= 0.26, win_size= 5)
+    vcl = curvilinearVelocity(data, fps= 30, pixel_size= 0.26)
 
     # Save the new data file with the statistics
     outputfile = csvfile.split('.')[0] + '_withstats.csv'
 
-    utils.saveDataFrame(average_speed, outputfile)
+    utils.saveDataFrame(vap, outputfile)
+    utils.saveDataFrame(vcl, outputfile)
 
     print("Statistics computed and saved to", outputfile)
 
