8366b5b0d9d49544e5b762a8708c4712716b4746
incrl <hartda23@ecu.edu>
2024-08-21 16:15:49 -0400

    Got basic slider functionality working

--- UnifiedFramework/app.py
+++ UnifiedFramework/app.py
@@ -1,10 +1,21 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+
 
 app = Flask(__name__, static_folder='static')
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/process', methods=["POST"])
+def process():
+    slider_value = request.form["slider"]
+    
+    templateData ={
+        'slider_value': slider_value
+    }
+    
+    return render_template('index.html', **templateData)
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1')
