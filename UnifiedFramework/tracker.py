193c15ded0d81f1a4d1a53692c13070f0c71ff46
Cameron Schmidt <schmidtc18@ecu.edu>
2024-08-30 14:50:01 -0400

    Updated tracking parameters

--- UnifiedFramework/tracker.py
+++ UnifiedFramework/tracker.py
@@ -76,7 +76,7 @@ def determineCentroids(frames, diameter=5, minmass=50, maxsize=10):
     
     return f
 
-def trackCentroids(f, search_range=7, memory=3, adaptive_stop=0.2, adaptive_step=0.95):
+def trackCentroids(f, search_range=9, memory=9, adaptive_stop=0.2, adaptive_step=0.95):
     t = tp.link(f, search_range=search_range, memory=memory, adaptive_stop=adaptive_stop, adaptive_step=adaptive_step)
     t = tp.filter_stubs(t, 15)
 
