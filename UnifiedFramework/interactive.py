d88032a5f7cff1dfa6450ffd09eb2571b643ec90
incrl <hartda23@ecu.edu>
2025-04-09 12:57:53 -0400

    Got interactive.py back up and running with new stats

--- UnifiedFramework/interactive.py
+++ UnifiedFramework/interactive.py
@@ -8,6 +8,9 @@ import time
 
 import utils
 
+import tkinter as tk
+from tkinter import filedialog
+
 current_sperm = None
 
 def onMouse(event, x, y, flags, param):
@@ -25,15 +28,24 @@ def onMouse(event, x, y, flags, param):
         for idx, sperm in current.iterrows():
 
             i = sperm['sperm']
-            x1 = sperm['bbox_x']
-            y1 = sperm['bbox_y']
-            x2 = sperm['bbox_x'] + sperm['bbox_w']
-            y2 = sperm['bbox_y'] + sperm['bbox_h']
+            
+            xc = int(sperm['x'])
+            yc = int(sperm['y'])
 
-            if x >= x1 and x <= x2 and y >= y1 and y <= y2:
+            if (x >= xc - 5 and x <= xc + 5) and (y >= yc - 5 and y <= yc + 5):
                 print(f'Sperm {i} clicked')
                 current_sperm = i
                 break
+
+            #x1 = sperm['bbox_x']
+            #y1 = sperm['bbox_y']
+            #x2 = sperm['bbox_x'] + sperm['bbox_w']
+            #y2 = sperm['bbox_y'] + sperm['bbox_h']
+
+            #if x >= x1 and x <= x2 and y >= y1 and y <= y2:
+            #    print(f'Sperm {i} clicked')
+            #    current_sperm = i
+            #    break
 
 def runInteractive(videofile, data):
 
@@ -77,14 +89,22 @@ def runInteractive(videofile, data):
         sperm = current[current['sperm'] == current_sperm]
         if len(sperm) > 0:
             sperm = sperm.iloc[0] # Fail safe for duplicate sperm ids
-            x = int(sperm['bbox_x'])
-            y = int(sperm['bbox_y'])
-            w = int(sperm['bbox_w'])
-            h = int(sperm['bbox_h'])
-            img = cv.rectangle(img, (x, y), (x + w, y + h), (0, 128, 0), 3)
+            xc = int(sperm['x'])
+            yc = int(sperm['y'])
+            img = cv.circle(img, (xc, yc), 5, (0, 255, 0), -1)
 
-            average_speed = sperm['average_speed']
+            #x = int(sperm['bbox_x'])
+            #y = int(sperm['bbox_y'])
+            #w = int(sperm['bbox_w'])
+            #h = int(sperm['bbox_h'])
+            #img = cv.rectangle(img, (x, y), (x + w, y + h), (0, 128, 0), 3)
 
+            #average_speed = sperm['average_speed']
+            vap = sperm['VAP']
+            vcl = sperm['VCL']
+            vsl = sperm['VSL']
+            alh = sperm['ALH_mean']
+            bcf = sperm['BCF']
 
         if current_sperm is not None:
 
@@ -95,9 +115,13 @@ def runInteractive(videofile, data):
             color = (255, 0, 0)
             thickness = 2
             text1 = f'Sperm {current_sperm}'
-            text2 = f'Average Speed: {average_speed:.2f} pixels/s'
+            text2 = f'VAP: {vap:.2f} microns/s'
+            text3 = f'VCL: {vcl:.2f} microns/s'
+            text4 = f'VSL: {vsl:.2f} microns/s'
             img = cv.putText(img, text1, org, font, fontScale, color, thickness, cv.LINE_AA)
             img = cv.putText(img, text2, (org[0], org[1]+50), font, fontScale, color, thickness, cv.LINE_AA)
+            img = cv.putText(img, text3, (org[0], org[1]+100), font, fontScale, color, thickness, cv.LINE_AA)
+            img = cv.putText(img, text4, (org[0], org[1]+150), font, fontScale, color, thickness, cv.LINE_AA)
 
 
         cv.imshow('Interactive', img)
@@ -114,11 +138,27 @@ def runInteractive(videofile, data):
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser(description='Show the tracked cells in a video')
-    parser.add_argument('videofile', type=str, help='Path to the video file')
-    parser.add_argument('csvfile', type=str, help='Path to the csvfile')
+    parser.add_argument('--videofile', type=str, help='Path to the video file')
+    parser.add_argument('--csvfile', type=str, help='Path to the csvfile')
 
     videofile = parser.parse_args().videofile
     csvfile = parser.parse_args().csvfile
+
+    if videofile is None:
+        root = tk.Tk()
+        root.withdraw()  # Hide the main window
+        videofile = filedialog.askopenfilename(title="Select the video file")
+
+        if videofile:
+            print("Selected file:", videofile)
+
+    if csvfile is None:
+        root = tk.Tk()
+        root.withdraw()  # Hide the main window
+        csvfile = filedialog.askopenfilename(title="Select the CSV file")
+
+        if csvfile:
+            print("Selected file:", csvfile)
 
     # Load dataframe
     dataframe = utils.loadDataFrame(csvfile,convert_segmentation=False)
